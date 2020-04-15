85e74c549eeba3ac6d94d5b61870fe0295d3a9a2
Alex Huang (ahuang1) <alex.huang@quest.com>
2020-04-15 17:53:06 +0800

    revert

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -1,13 +1,4 @@
 
-$ModulePath = Split-Path -Path $PSScriptRoot -Parent
-$ModuleName = Split-Path -Path $ModulePath -Leaf
-
-# Make sure one or multiple versions of the module are not loaded
-Get-Module -Name $ModuleName | Remove-Module
-
-# Import the module and store the information about the module
-Import-Module -Name "$ModulePath\$ModuleName.psd1" -PassThru
-
 $ModulePath = $PSScriptRoot.Replace('\tool', '')
 $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
 $NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
