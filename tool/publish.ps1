048c4dce3a65520933a6aba014abd9d9877ce904
Alex Huang (ahuang1) <alex.huang@quest.com>
2020-04-15 17:23:03 +0800

    fix publish script

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -5,7 +5,11 @@ $NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
 $NewModulePath += ";$ModulePath"
 [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
 Write-Host $NewModulePath
+
+Import-Module -Name SimpleRequest -PassThru
 $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
+
 Publish-Module -Name SimpleRequest -NuGetApiKey $key
 
 [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
+Remove-Module -Name SimpleRequest
