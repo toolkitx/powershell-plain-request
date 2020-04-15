ed39c31e6c07b3294aa453259d057e8686c1fdb8
Alex Huang (ahuang1) <alex.huang@quest.com>
2020-04-15 16:15:23 +0800

    add publish workflow

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -6,6 +6,6 @@ $NewModulePath += ";$ModulePath"
 [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
 Write-Host $NewModulePath
 
-Publish-Module -Name SimpleRequest -NuGetApiKey yourkey
+Publish-Module -Name SimpleRequest -NuGetApiKey [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
 
 [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
