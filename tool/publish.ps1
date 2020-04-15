7590f79c8a913656ec8610c90fde1811834b52d5
Alex Huang (ahuang1) <alex.huang@quest.com>
2020-04-15 17:32:08 +0800

    update

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -1,4 +1,13 @@
 
+$ModulePath = Split-Path -Path $PSScriptRoot -Parent
+$ModuleName = Split-Path -Path $ModulePath -Leaf
+
+# Make sure one or multiple versions of the module are not loaded
+Get-Module -Name $ModuleName | Remove-Module
+
+# Import the module and store the information about the module
+Import-Module -Name "$ModulePath\$ModuleName.psd1" -PassThru
+
 $ModulePath = $PSScriptRoot.Replace('\tool', '')
 $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
 $NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
@@ -6,10 +15,8 @@ $NewModulePath += ";$ModulePath"
 [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
 Write-Host $NewModulePath
 
-Import-Module -Name .\SimpleRequest\SimpleRequest.psd1 -PassThru
 $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
 
 Publish-Module -Name SimpleRequest -NuGetApiKey $key
 
 [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
-Remove-Module -Name SimpleRequest
