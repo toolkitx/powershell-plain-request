587abdc3c2d550306a17bc1ab00f9cfca66d354c
Alex <alex@Alexs-MacBook-Pro.local>
2020-04-15 22:50:20 +0800

    update

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -4,7 +4,13 @@ $galleryVersion = (Find-Module -Name SimpleRequest).Version
 Write-Host "Gallery: v$galleryVersion"
 $ModuleName = "SimpleRequest"
 Get-Module -Name $ModuleName | Remove-Module
-Import-Module -Name "$ModulePath/$ModuleName"
+
+$ModuleInstallPath = "$ModulePath/$ModuleName"
+
+Write-Host $ModuleInstallPath
+
+Import-Module -Name $ModuleInstallPath -Scope Global
+
 $localVersion = (Get-Module -Name $ModuleName).Version.ToString()
 Write-Host "New Version: v$localVersion"
 if ($galleryVersion -eq $localVersion) {
@@ -16,3 +22,14 @@ $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
 
 Publish-Module -Name SimpleRequest -NuGetApiKey $key
 Write-Host "Published: v$localVersion"
+
+# $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
+# $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
+# $NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
+# $NewModulePath += ";$ModulePath"
+# [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
+# Write-Host $NewModulePath
+
+# Publish-Module -Name SimpleRequest -NuGetApiKey test
+
+# [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
