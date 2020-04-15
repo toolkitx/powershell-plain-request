ebbf26b7968f8c6e680d24cbb78d7257619a1fc6
Alex <alex@Alexs-MacBook-Pro.local>
2020-04-15 23:02:05 +0800

    update

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -1,36 +1,24 @@
-# $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
+$ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
 
-# $galleryVersion = (Find-Module -Name SimpleRequest).Version
-# Write-Host "Gallery: v$galleryVersion"
-# $ModuleName = "SimpleRequest"
-# Get-Module -Name $ModuleName | Remove-Module
-
-# $ModuleInstallPath = "$ModulePath/$ModuleName"
-
-# Write-Host $ModuleInstallPath
-
-# Import-Module -Name $ModuleInstallPath -Scope Global
-
-# $localVersion = (Get-Module -Name $ModuleName).Version.ToString()
-# Write-Host "New Version: v$localVersion"
-# if ($galleryVersion -eq $localVersion) {
-#     Write-Host "Same version, skip!"
-#     return;
-# }
-
-# $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
-
-# Publish-Module -Name SimpleRequest -NuGetApiKey $key
-# Write-Host "Published: v$localVersion"
+$galleryVersion = (Find-Module -Name SimpleRequest).Version
+Write-Host "Gallery: v$galleryVersion"
 
 $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
 $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
 $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
 Write-Host "Origin Module Path: $OriginalModulePath"
-$NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
-$NewModulePath += ";$ModulePath"
+$NewModulePath = $OriginalModulePath.Replace(":$ModulePath", "")
+$NewModulePath += ":$ModulePath"
 [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
+
 Write-Host "New Module Path: $NewModulePath"
+
+$localVersion = (Get-Module -Name $ModuleName).Version.ToString()
+Write-Host "New Version: v$localVersion"
+if ($galleryVersion -eq $localVersion) {
+    Write-Host "Same version, skip!"
+    return;
+}
 
 Publish-Module -Name "SimpleRequest" -NuGetApiKey $key
 
