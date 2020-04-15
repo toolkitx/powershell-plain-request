66c7bf9550dee00769b3f738cee1682c3d9bad21
Alex <alex@Alexs-MacBook-Pro.local>
2020-04-15 22:58:14 +0800

    update

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -1,35 +1,37 @@
-$ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
+# $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
 
-$galleryVersion = (Find-Module -Name SimpleRequest).Version
-Write-Host "Gallery: v$galleryVersion"
-$ModuleName = "SimpleRequest"
-Get-Module -Name $ModuleName | Remove-Module
+# $galleryVersion = (Find-Module -Name SimpleRequest).Version
+# Write-Host "Gallery: v$galleryVersion"
+# $ModuleName = "SimpleRequest"
+# Get-Module -Name $ModuleName | Remove-Module
 
-$ModuleInstallPath = "$ModulePath/$ModuleName"
+# $ModuleInstallPath = "$ModulePath/$ModuleName"
 
-Write-Host $ModuleInstallPath
+# Write-Host $ModuleInstallPath
 
-Import-Module -Name $ModuleInstallPath -Scope Global
+# Import-Module -Name $ModuleInstallPath -Scope Global
 
-$localVersion = (Get-Module -Name $ModuleName).Version.ToString()
-Write-Host "New Version: v$localVersion"
-if ($galleryVersion -eq $localVersion) {
-    Write-Host "Same version, skip!"
-    return;
-}
+# $localVersion = (Get-Module -Name $ModuleName).Version.ToString()
+# Write-Host "New Version: v$localVersion"
+# if ($galleryVersion -eq $localVersion) {
+#     Write-Host "Same version, skip!"
+#     return;
+# }
+
+# $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
+
+# Publish-Module -Name SimpleRequest -NuGetApiKey $key
+# Write-Host "Published: v$localVersion"
 
 $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
+$ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
+$OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
+Write-Host "Origin Module Path: $OriginalModulePath"
+$NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
+$NewModulePath += ";$ModulePath"
+[Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
+Write-Host "New Module Path: $NewModulePath"
 
-Publish-Module -Name SimpleRequest -NuGetApiKey $key
-Write-Host "Published: v$localVersion"
+Publish-Module -Name "SimpleRequest" -NuGetApiKey $key
 
-# $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
-# $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
-# $NewModulePath = $OriginalModulePath.Replace(";$ModulePath", "")
-# $NewModulePath += ";$ModulePath"
-# [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
-# Write-Host $NewModulePath
-
-# Publish-Module -Name SimpleRequest -NuGetApiKey test
-
-# [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
+[Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
