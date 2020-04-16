e8a1e24ccacc4a9d1c6da2e199aa963147dd6592
Alex Huang (ahuang1) <alex.huang@quest.com>
2020-04-16 09:10:31 +0800

    compare version before publish

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -1,28 +1,31 @@
 $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
+$ModuleName = "SimpleRequest"
+$galleryVersion = (Find-Module -Name $ModuleName).Version
+Write-Host "===> Gallery: v$galleryVersion"
 
-$galleryVersion = (Find-Module -Name SimpleRequest).Version
-Write-Host "Gallery: v$galleryVersion"
-
-$key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
 $ModulePath = $PSScriptRoot.Replace('\tool', '').Replace('/tool', '')
 $OriginalModulePath = [Environment]::GetEnvironmentVariable("PSModulePath")
-Write-Host "Origin Module Path: $OriginalModulePath"
+Write-Host "===> Origin Module Path: $OriginalModulePath"
 $NewModulePath = $OriginalModulePath.Replace(":$ModulePath", "")
 $NewModulePath += ":$ModulePath"
 [Environment]::SetEnvironmentVariable("PSModulePath", $NewModulePath)
 
-Write-Host "New Module Path: $NewModulePath"
+Write-Host "===> New Module Path: $NewModulePath"
+$localModule = Import-PowerShellDataFile -Path "$ModulePath/$ModuleName/$ModuleName.psd1"
+if ($localModule) {
+    $localVersion = $localModule.ModuleVersion.ToString()
+    Write-Host "===> New Version: v$localVersion"
+}
 
-$localVersion = (Get-Module -Name SimpleRequest).Version.ToString()
-Write-Host "New Version: v$localVersion"
 
 if ($galleryVersion -eq $localVersion) {
-    Write-Host "Same version, skip!"
+    Write-Host "===> Same version, skip!"
     return;
 } else {
-    Publish-Module -Name SimpleRequest -NuGetApiKey $key
-    $galleryVersion = (Find-Module -Name SimpleRequest).Version
-    Write-Host "Current Gallery Published: v$galleryVersion"
+    $key = [Environment]::GetEnvironmentVariable("PSGalleryAPIKey")
+    Publish-Module -Name $ModuleName -NuGetApiKey $key
+    $newGalleryVersion = (Find-Module -Name $ModuleName).Version
+    Write-Host "===> Current Gallery Published: v$galleryVersion => v$newGalleryVersion"
 }
 
 [Environment]::SetEnvironmentVariable("PSModulePath", $OriginalModulePath)
